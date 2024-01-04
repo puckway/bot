0f6ffd49007fe205af5b2ae36d53345f1d9d9d15
shay <43248357+shayypy@users.noreply.github.com>
2024-01-04 08:37:44 -0600

    fix(bot): fallback for teams with no emoji

--- packages/bot/src/util/emojis.ts
+++ packages/bot/src/util/emojis.ts
@@ -1,4 +1,9 @@
 import { Env } from "..";
 
-export const khlTeamEmoji = (env: Env, team: { id: number }) =>
-  `<:_:${env[`KHL_TEAM_EMOJI_${team.id}` as keyof Env]}>`;
+export const khlTeamEmoji = (env: Env, team: { id: number }) => {
+  const key = `KHL_TEAM_EMOJI_${team.id}`;
+  if (key in env) {
+    return `<:_:${env[key as keyof Env]}>`;
+  }
+  return "❔";
+};
