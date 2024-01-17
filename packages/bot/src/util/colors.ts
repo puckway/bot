e095d6dbb73a7432ec7346721e9395212afd8e6b
shay <43248357+shayypy@users.noreply.github.com>
2024-01-16 23:26:14 -0600

    feat(bot): pwhl team colors

--- packages/bot/src/util/colors.ts
+++ packages/bot/src/util/colors.ts
@@ -1,5 +1,15 @@
+export type PwhlTeamId = "1" | "2" | "3" | "4" | "5" | "6";
+
 export const colors = {
   main: 0x985df5,
   pwhl: 0x2e0887,
+  pwhlTeams: {
+    "1": 0x244635,
+    "2": 0x2b1b44,
+    "3": 0x7b2d35,
+    "4": 0x4fafa8,
+    "5": 0x982932,
+    "6": 0x467ddb,
+  } as Record<PwhlTeamId, number>,
   khl: 0x306da9,
 };
