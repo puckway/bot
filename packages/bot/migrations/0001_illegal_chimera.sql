5b66190fe8bcb285bff2bb2ea4561feb4c895ea6
shay <43248357+shayypy@users.noreply.github.com>
2024-01-20 16:11:48 -0600

    fix(bot): migration logs for cloudflare migration

--- packages/bot/migrations/0001_illegal_chimera.sql
+++ packages/bot/migrations/0001_illegal_chimera.sql
@@ -7,13 +7,4 @@ CREATE TABLE `games` (
 	`postedPreview` integer DEFAULT false NOT NULL,
 	`postedFinal` integer DEFAULT false NOT NULL
 );
---> statement-breakpoint
-/*
- SQLite does not support "Set default to column" out of the box, we do not generate automatic migration for that, so it has to be done manually
- Please refer to: https://www.techonthenet.com/sqlite/tables/alter_table.php
-                  https://www.sqlite.org/lang_altertable.html
-                  https://stackoverflow.com/questions/2083543/modify-a-columns-type-in-sqlite3
-
- Due to that we don't generate migration automatically and it has to be done manually
-*/--> statement-breakpoint
 CREATE UNIQUE INDEX `games_league_nativeId_unique` ON `games` (`league`,`nativeId`);
