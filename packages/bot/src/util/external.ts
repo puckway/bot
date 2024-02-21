624e8d31fb5173127aa3d09d66359570cf969518
shay <43248357+shayypy@users.noreply.github.com>
2024-02-20 21:33:27 -0600

    feat(bot): external league utils

---
+++ packages/bot/src/util/external.ts
@@ -0,0 +1,51 @@
+import { League } from "../db/schema";
+
+export const getExternalUtils = <L = League>(league: L, locale_?: string) => {
+  const locale = locale_ ?? "en";
+  switch (league) {
+    // case "mhl":
+    // case "zhhl":
+    case "khl": {
+      const site =
+        league === "khl"
+          ? `https://${locale === "ru" ? "www" : locale}.khl.ru`
+          : league === "mhl"
+            ? locale === "en"
+              ? "https://engmhl.khl.ru"
+              : "https://mhl.khl.ru"
+            : "https://whl.khl.ru";
+      return {
+        site,
+        player: (playerId: string | number) => `${site}/players/${playerId}`,
+        club: (slug: string) => `${site}/clubs/${slug}`,
+        gameCenter: (gameId: string, seasonId?: string) =>
+          `${site}/game/${seasonId}/${gameId}/resume/`,
+        standings: (seasonId?: string, part?: string) =>
+          seasonId
+            ? `${site}/standings/${seasonId}/${part ?? "conference"}/`
+            : `${site}/standings/`,
+      };
+    }
+    case "pwhl": {
+      const site = `https://www.thepwhl.com/${locale}`;
+      return {
+        site,
+        player: (playerId: string | number) =>
+          `${site}/stats/${locale === "fr" ? "joueur" : "player"}/${playerId}`,
+        teamRoster: (teamId: string | number, seasonId?: string | number) =>
+          `${site}/stats/${
+            locale === "fr" ? "alignement" : "roster"
+          }/${teamId}/${seasonId ?? ""}`,
+        teamSite: (subdomain: string) => `https://${subdomain}.thepwhl.com`,
+        gameCenter: (gameId: string) =>
+          `${site}/stats/${
+            locale === "fr" ? "game-center" : "game-centre"
+          }/${gameId}`,
+      };
+    }
+    default:
+      throw Error("League not supported. No utilities are available.");
+  }
+};
+
+export type ExternalUtils<L = League> = ReturnType<typeof getExternalUtils<L>>;
